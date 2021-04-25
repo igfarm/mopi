a0c5c4e23d981da3293f1622f46e6387e37e2e0e
Jaime Pereira <jaime@iguana-farm.com>
2021-04-25 22:35:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+install aur
+
+
+  226  bash <(curl -s archibold.io/install/aur)
+
+
+create user
+
+add user to sudo
+
+install pigpio-git
+
+   16  AUR_NO_SUDO=1 aur pigpio-git
+
+install node app
+
+   18  npm install raspi-io
+   47  npm install johnny-five oled-js oled-font-5x7
+   49  node test.js 
+
+
---
+++ oled.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+const five = require('johnny-five');
+const font = require('oled-font-5x7');
+const Oled = require('oled-js');
+const Raspi = require('raspi-io').RaspiIO;
+const board = new five.Board({io: new Raspi});
+board.on('ready', () => {
+  console.log('Connected to Raspberry, ready.');
+  const opts = {
+    width: 128,
+    height: 32,
+    address: 0x3c
+  };
+
+	const interval = 1000;
+  const oled = new Oled(board, five, opts);
+
+	function update() {
+
+fs.readFile('/proc/asound/card0/stream0', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+ let match = data.match(/Momentary freq = ([0-9]+) Hz/);
+	let rate = "--";
+	if (match && match.length > 1) {
+	rate = match[1];
+	rate = rate / 1000;
+	rate = rate.toFixed(1);
+	rate = rate.replace(".0", "");
+	rate = rate.toString() + "kHz";
+	}
+
+  oled.clearDisplay();
+  oled.setCursor(1, 1);
+  oled.writeString(font, 2, rate, 1, true, 2);
+  oled.update();
+   setTimeout(update, interval);
+})
+
+
+
+	}
+
+   update();
+});
+
+
+
